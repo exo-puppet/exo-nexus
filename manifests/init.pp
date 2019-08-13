d5771f11babc97fcfc89029f0f2afb001c8a15ba
Vincent Sellier <vsellier@exoplatform.com>
2019-08-13 16:18:23 +0200

    ITOP-4280 upgrade to nexus 2.14.14

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class nexus (
   $image              = 'sonatype/nexus',
-  $version            = 'pro-2.14.13-01',
+  $version            = 'pro-2.14.14-01',
   $install_dir        = '/opt/nexus',
   $manage_install_dir = 'true',
   $data_dir           = '/srv/nexus',
