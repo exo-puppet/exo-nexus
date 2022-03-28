6f1705d203e80691af2d8acedbda0bbc0d828a68
Houssem Ben Ali <hbenali@exoplatform.com>
2022-03-28 12:38:12 +0100

    Fix nexus configuration file user permission

--- manifests/init.pp
+++ manifests/init.pp
@@ -53,8 +53,8 @@ class nexus (
   ########################
   file { "${nexus::install_dir}/conf/nexus.properties" :
     ensure  => present,
-    owner   => $account,
-    group   => $account,
+    owner   => 'nexus',
+    group   => 'nexus',
     mode    => '0644',
     content => template ('nexus/conf/nexus.properties.erb'),
     require => [File[$nexus::install_dir]],
